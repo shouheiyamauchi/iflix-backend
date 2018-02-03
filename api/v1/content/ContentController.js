f4bf7b7afb87cd3d7d7b94761e28f7a175c97c76
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2018-02-03 16:20:27 +1100

    refactor and extract out destroy method

--- api/v1/content/ContentController.js
+++ api/v1/content/ContentController.js
@@ -2,6 +2,8 @@ const Content = require('./contentModel');
 const apiService = require(__servicesDir + 'api');
 const mongoDbService = require(__servicesDir + 'mongoDb');
 
+// router functions
+
 const list = (req, res) => {
   Content.find({}, (mongoErrors, contents) => {
     const statusCode = mongoErrors ? 500 : 200;
@@ -43,25 +45,20 @@ const update = (req, res) => {
 };
 
 const destroy = (req, res) => {
-  let statusCode = null;
   const errors = {};
-  let result = '';
+  const result = {};
 
-  Content.findByIdAndRemove(req.params.id, (mongoErrors, content) => {
-    if (mongoErrors) {
-      statusCode = 500;
-      mongoDbService.convertMongoErrors(mongoErrors, errors);
-    } else if (!content) {
-      statusCode = 404;
-      errors.notFound = { 'message': 'Content with that ID not found.'};
-    } else {
-      statusCode = 200;
-      result = content.title + ' was deleted.';
-    };
-
-    apiService.sendResponse(res, statusCode, result, errors);
-  });
+  findAndDestroyContent(req.params.id, errors)
+    .then(content => {
+      const statusCode = 200;
+      result.message = content.title + ' was deleted.';
+      apiService.sendResponse(res, statusCode, result, errors);
+    }).catch(errorStatusCode => {
+      apiService.sendResponse(res, errorStatusCode, result, errors);
+    });
 };
+
+// helper functions
 
 const setContentValues = (req, content) => {
   const { title, genre, releaseDate, thumbnail } = req.query;
@@ -94,7 +91,7 @@ const findContent = async (id, errors) => {
 
       if (!searchResult) {
         errorStatusCode = 404;
-        errors.notFound = { 'message': 'Content with that ID not found.'};
+        errors.notFound = { 'message': 'Content with ID ' + id + ' not found.'};
       };
     }).catch(mongoErrors => {
       errorStatusCode = 500;
@@ -110,5 +107,32 @@ const findContent = async (id, errors) => {
     };
   });
 };
+
+const findAndDestroyContent = async (id, errors) => {
+  let errorStatusCode = null;
+  let searchResult = {};
+
+  await Content.findByIdAndRemove(id)
+    .then(content => {
+      searchResult = content;
+
+      if (!searchResult) {
+        errorStatusCode = 404;
+        errors.notFound = { 'message': 'Content with ID ' + id + ' not found.'};
+      };
+    }).catch(mongoErrors => {
+      errorStatusCode = 500;
+      mongoDbService.convertMongoErrors(mongoErrors, errors);
+    });
+
+
+  return new Promise((resolve, reject) => {
+    if (!errorStatusCode) {
+      resolve(searchResult);
+    } else {
+      reject(errorStatusCode);
+    };
+  });
+}
 
 module.exports = { list, show, create, update, destroy };
